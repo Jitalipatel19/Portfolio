1937641d2059426e43ca05f7eb2c2c6a1999c524
Jitali Patel <140070393+Jitalipatel19@users.noreply.github.com>
2024-06-03 13:47:31 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import project1 from "./assets/projects/portfolio.png";
-import project2 from "./assets/projects/project-2.jpg";
-import project3 from "./assets/projects/project-3.jpg";
-import project4 from "./assets/projects/project-4.jpg";
+import project2 from "./assets/projects/internify.png";
+import project3 from "./assets/projects/amazon.png";
+import project4 from "./assets/projects/llh.png";
+import project5from "./assets/projects/ttc.png";
+import project6 from "./assets/projects/number_guessing.png";
+import project7 from "./assets/projects/basic_activities.jpeg";
+import project7 from "./assets/projects/storage_management.jpeg";
 
 export const HERO_CONTENT = `I am a passionate Software developer with a knack for crafting robust and scalable web applications. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
@@ -10,7 +14,7 @@ export const ABOUT_TEXT = `I am a dedicated and versatile Software developer wit
 export const PROJECTS = [
   {
     title: "Portfolio",
-    image: project4,
+    image: project1,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
     technologies: ["ReactJS", "Tailwind CSS", "Framer Motion"],
@@ -18,56 +22,56 @@ export const PROJECTS = [
   {
     
     title: "Internify",
-    image: project1,
+    image: project2,
     description:
       "Keep Record of Student's Internships",
     technologies: ["PHP", "HTML", "CSS", "JS"],
   },
   {
     title: "Amazon Frontend",
-    image: project4,
+    image: project3,
     description:
       "This is small amazon clone in which frontend designed",
     technologies: ["HTML", "CSS"],
   },
   {
     title: "Hostel Management",
-    image: project2,
+    image: project4,
     description:
       "This is hostel management in which students who get admission in another city's collage/school they can book hostel room online.",
     technologies: ["PHP","HTML", "CSS", "JS"],
   },
   {
     title: "Tic Tac Toe",
-    image: project3,
+    image: project5,
     description:
       "This is Tic Tac Toe Game using android in which to players can play the game and see the scores as well as reset the game",
     technologies: ["JAVA", "XML"],
   },
   {
     title: "Random Number Gussesing",
-    image: project4,
+    image: project6,
     description:
       "This is number guessing game using android in which one random number will generate and that number player should guess that number",
     technologies: ["JAVA", "XML"],
   },
   {
     title: "Dice Rolling Game",
-    image: project4,
+    image: project7,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "This is simple dice rolling game using android.",
+    technologies: ["JAVA", "XML"],
   },
   {
     title: "Basic Activities",
-    image: project4,
+    image: project8,
     description:
       "Basic Activities",
     technologies: ["JAVA", "XML"],
   },
   {
     title: "Storage Management",
-    image: project4,
+    image: project9,
     description:
       "This is storage management application.In which user can use mobile's storage",
       technologies: ["JAVA", "XML"],
@@ -75,7 +79,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "Valsad,Gujarat ",
+  address: "Valsad,Gujarat",
   phoneNo: "+12 4555 666 00 ",
   email: "me@example.com",
 };
