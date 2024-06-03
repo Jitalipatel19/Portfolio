1a7a1b23d5b5cd52f87841246cb8af903e9fb7a0
Jitali Patel <140070393+JitaliP@users.noreply.github.com>
2024-06-03 13:31:03 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,81 @@
+import project1 from "./assets/projects/portfolio.png";
+import project2 from "./assets/projects/project-2.jpg";
+import project3 from "./assets/projects/project-3.jpg";
+import project4 from "./assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate Software developer with a knack for crafting robust and scalable web applications. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile Software developer with a passion for creating efficient and user-friendly web applications. I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const PROJECTS = [
+  {
+    title: "Portfolio",
+    image: project4,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["ReactJS", "Tailwind CSS", "Framer Motion"],
+  },
+  {
+    
+    title: "Internify",
+    image: project1,
+    description:
+      "Keep Record of Student's Internships",
+    technologies: ["PHP", "HTML", "CSS", "JS"],
+  },
+  {
+    title: "Amazon Frontend",
+    image: project4,
+    description:
+      "This is small amazon clone in which frontend designed",
+    technologies: ["HTML", "CSS"],
+  },
+  {
+    title: "Hostel Management",
+    image: project2,
+    description:
+      "This is hostel management in which students who get admission in another city's collage/school they can book hostel room online.",
+    technologies: ["PHP","HTML", "CSS", "JS"],
+  },
+  {
+    title: "Tic Tac Toe",
+    image: project3,
+    description:
+      "This is Tic Tac Toe Game using android in which to players can play the game and see the scores as well as reset the game",
+    technologies: ["JAVA", "XML"],
+  },
+  {
+    title: "Random Number Gussesing",
+    image: project4,
+    description:
+      "This is number guessing game using android in which one random number will generate and that number player should guess that number",
+    technologies: ["JAVA", "XML"],
+  },
+  {
+    title: "Dice Rolling Game",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+  {
+    title: "Basic Activities",
+    image: project4,
+    description:
+      "Basic Activities",
+    technologies: ["JAVA", "XML"],
+  },
+  {
+    title: "Storage Management",
+    image: project4,
+    description:
+      "This is storage management application.In which user can use mobile's storage",
+      technologies: ["JAVA", "XML"],
+  },
+];
+
+export const CONTACT = {
+  address: "Valsad,Gujarat ",
+  phoneNo: "+12 4555 666 00 ",
+  email: "me@example.com",
+};
